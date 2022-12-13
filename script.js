0b4083129043b3045e9c71d3f295f196abbe9e5d
bszeidl <szeidl.balazs@hotmail.com>
2022-12-13 11:20:07 +0100

    The 2 overlapping rectangles are ready.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+    let size = 100;
+
+    context.fillStyle = "rgba(255,0,0,.5)";
+    context.fillRect(canvasWidth *.5 - size * .75,canvasHeight * .5 - size * .75,size,size);
+    
+    context.fillStyle = "rgba(0,0,255,.5)";
+    context.fillRect(canvasWidth *.5 - size * .25,canvasHeight * .5 - size * .255,size,size);  
 
 }
 
